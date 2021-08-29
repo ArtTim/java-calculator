9af78a042fa0d543eeea0ad92896ad9b0d246626
ArtTim <tim-murr@inbox.ru>
2021-08-29 23:22:05 +0300

    Add new project

---
+++ src/calc/Calc.java
@@ -0,0 +1,59 @@
+package calc;
+
+import treatment.CheckNum;
+import treatment.Convertation;
+import treatment.Request;
+
+public class Calc {
+    Request request = new Request();
+    CheckNum checkNum = new CheckNum();
+    Convertation convertation = new Convertation();
+    int result;
+
+    public void calculate() { // Сам калькулятор. Инициализация запроса арифметической операции, и его расчёт.
+        request.getRequest();
+        checkNum.checkString(request.firstOperand, request.secondOperand);
+            if (checkNum.positiveNumber) {
+                if (checkNum.isArabic) { // Если арабские
+                    System.out.println(request.firstOperand.toUpperCase() + " " + request.operator + " " + request.secondOperand.toUpperCase() + " = " + arabicCalc() + ".");
+                } else if (checkNum.isRoman) { // Если римские
+                    System.out.println(request.firstOperand.toUpperCase() + " " + request.operator + " " + request.secondOperand.toUpperCase() + " = " + romanCalc() + ".");
+                }
+            } else { // Ошибка
+                System.out.println("Ожидаются числа от 1 до 10 в арабской либо римской системе исчисления.");
+            }
+    }
+
+    // Калькулятор арабских чисел
+    public int arabicCalc() {
+        int firstOperand = Integer.parseInt(request.firstOperand),
+                secondOperand = Integer.parseInt(request.secondOperand);
+            if (request.operator.equals("+")) {
+                result = firstOperand + secondOperand;
+            } else if (request.operator.equals("-")) {
+                result = firstOperand - secondOperand;
+            } else if (request.operator.equals("*")) {
+                result = firstOperand * secondOperand;
+            } else if (request.operator.equals("/")) {
+                result = firstOperand / secondOperand;
+            }
+            return result;
+    }
+
+    // Калькулятор римских чисел
+    public String romanCalc() {
+        int firstOperand = convertation.toArabian(request.firstOperand),
+                secondOperand = convertation.toArabian(request.secondOperand);
+        if (request.operator.equals("+")) {
+            result = firstOperand + secondOperand;
+        } else if (request.operator.equals("-")) {
+            result = firstOperand - secondOperand;
+        } else if (request.operator.equals("*")) {
+            result = firstOperand * secondOperand;
+        } else if (request.operator.equals("/")) {
+            result = firstOperand / secondOperand;
+        }
+        return convertation.toRoman(result);
+    }
+
+}
---
+++ src/treatment/CheckNum.java
@@ -0,0 +1,50 @@
+package treatment;
+
+public class CheckNum {
+    private String [] arabic = {"10","1","2","3","4","5","6","7","8","9"};
+    private String [] roman = {"X","I","II","III","IV","V","VI","VII","VIII","IX"};
+    private String [] romanNumerous = {"C", "X","XX","XXX","XL","L","LX","LXX","LXXX"};
+    public boolean firstIsArabic = false, secondIsArabic = false, firstIsRoman = false, secondIsRoman = false, positiveNumber = false, isArabic = false, isRoman = false;
+
+    public void checkString(String firstOperand, String secondOperand ) { // Проверяется соответствует ли число массиву arabic или roman
+        for (int i = 0; i < arabic.length; i++) {
+            String arabianStr = arabic[i];
+            String romanStr = roman[i];
+            if (firstOperand.equals(arabianStr)) {
+                firstIsArabic = true;
+            }
+            if (secondOperand.equals(arabianStr)) {
+                secondIsArabic = true;
+            }
+            if (firstOperand.toUpperCase().equals(romanStr)) {
+                firstIsRoman = true;
+            }
+            if (secondOperand.toUpperCase().equals(romanStr)) {
+                secondIsRoman = true;
+            }
+        }
+
+        // Проверка на число 1 до 10
+        if (firstIsArabic && secondIsArabic) { // Если оба числа арабские
+            if (Integer.valueOf(firstOperand) > 0 && Integer.valueOf(firstOperand) <= 11 && Integer.valueOf(secondOperand) > 0 && Integer.valueOf(secondOperand) <= 11) {
+                positiveNumber = true;
+            }
+            isArabic = true;
+        } else if (firstIsRoman && secondIsRoman) { // Если оба числа римские
+            positiveNumber = true;
+            isRoman = true;
+        } else {
+            positiveNumber = false;
+        }
+    }
+
+    public String[] getArabic() {
+        return arabic;
+    }
+    public String[] getRoman() {
+        return roman;
+    }
+    public String[] getRomanNumerous() {
+        return romanNumerous;
+    }
+}
